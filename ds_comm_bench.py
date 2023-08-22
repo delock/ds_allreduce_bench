f9500d30858af472308b7f6baed2b1c2b1180cf2
Ma, Guokai <guokai.ma@intel.com>
2023-08-22 13:17:22 +0000

    support latest deepspeed comm interface

--- ds_comm_bench.py
+++ ds_comm_bench.py
@@ -5,28 +5,34 @@ import time
 
 deepspeed.init_distributed()
 
+def alloc_tensors(use_dtype):
+    a = torch.ones(1024, 1024, dtype=torch.bfloat16)
+    c = torch.ones(1024, 1024, dtype=torch.bfloat16)
+    #t = torch.ones(6144, 4, dtype=use_dtype) * (dist.get_rank()+1.0)
+    #t = torch.ones(14336, 4, dtype=use_dtype) * (dist.get_rank()+1.0)
+    #t = torch.ones(1024, 10, dtype=use_dtype) * (dist.get_rank()+1.0)
+    t = torch.ones(1024, 5, dtype=use_dtype) * (dist.get_rank()+1.0)
+    #t = torch.ones(1024, 512, dtype=use_dtype) * (dist.get_rank()+1.0)
+    return a, c, t
+
 def test_allreduce(reuse_buffer, use_dtype, loop_count):
     b = torch.ones(1024, 1024, dtype=torch.bfloat16)
     if reuse_buffer:
-        a = torch.ones(1024, 1024, dtype=torch.bfloat16)
-        c = torch.ones(1024, 1024, dtype=torch.bfloat16)
-        t = torch.ones(1024, 5, dtype=use_dtype)
+        a,c,t = alloc_tensors(use_dtype)
     t_total = 0.0
     start = time.time()
     for i in range(loop_count):
         if not reuse_buffer:
-            a = torch.ones(1024, 1024, dtype=torch.bfloat16)
-            c = torch.ones(1024, 1024, dtype=torch.bfloat16)
-            t = torch.ones(1024, 5, dtype=use_dtype)
-        torch.matmul(a, b, out=c)
-        torch.matmul(a, b, out=c)
+            a,c,t = alloc_tensors(use_dtype)
+        #torch.matmul(a, b, out=c)
+        #torch.matmul(a, b, out=c)
         #v = 0.0000001
         #for i in range(100000):
         #    v = v + 0.01
         dist.barrier(t)
         t0 = time.time()
-        #dist.all_reduce_low_latency(t)
-        dist.all_reduce(t)
+        dist.inference_all_reduce(t, async_op=False)
+        #dist.all_reduce(t)
         t1 = time.time()
         if (i==0 and dist.get_rank()==0):
             print (t)
@@ -35,11 +41,14 @@ def test_allreduce(reuse_buffer, use_dtype, loop_count):
     #print (f'total elapsed {end-start}')
     return t_total
 
-loop_count = 17920
-t = test_allreduce(True, torch.bfloat16, loop_count)
+loop_count = 579200
+t0 = time.time()
+t = test_allreduce(False, torch.bfloat16, loop_count)
+t1 = time.time()
+#t = test_allreduce(False, torch.float32, loop_count)
+#t = test_allreduce(False, torch.float16, loop_count)
 
 if dist.get_rank() == 0:
-    print (f'duration = {t}s')
-    print (f'avg duration = {t/(loop_count/1000.0)}ms')
+    print (f'avg duration = {t/(loop_count/1000.0)}ms, E2E={t1-t0}')
 
 #dist.log_summary(show_straggler=True)
--- run_comm_bench.sh
+++ run_comm_bench.sh
@@ -19,8 +19,20 @@ export CCL_WORKER_COUNT=1
 #8 ranks
 #PSM3_DEVICES=self,nic
 
+#for 48 core *2
 export CCL_WORKER_AFFINITY=10,22,34,46,58,70,82,94
 deepspeed --bind_cores_to_rank --bind_core_list 0-9,12-21,24-33,36-45,48-57,60-69,72-81,84-93 ds_comm_bench.py
+#for 56 core *2
+#export CCL_WORKER_AFFINITY=12,26,40,54,68,82,96,110
+#deepspeed --bind_cores_to_rank --bind_core_list 0-11,14-25,28-39,42-53,56-67,70-81,84-95,98-109 ds_comm_bench.py
+#mpirun -ppn 8 ~/oneCCL/build/_install/examples/benchmark/benchmark -i 17920 -y 24576 -d bfloat16
+
+#export CCL_WORKER_AFFINITY=48,104
+#deepspeed --num_accelerators 2 --bind_cores_to_rank --bind_core_list 0-39,48-87 ds_comm_bench.py
+#deepspeed --num_accelerators 2 --bind_cores_to_rank --bind_core_list 0-47,56-103 ds_comm_bench.py
+#mpirun -ppn 2 ~/oneCCL/build/_install/examples/benchmark/benchmark -i 17920 -y 57344 -d bfloat16
+
+#deepspeed --num_accelerators 10 --bind_cores_to_rank ds_comm_bench.py
 
 #export CCL_WORKER_AFFINITY=10,58
 #deepspeed --num_accelerator 2 --bind_cores_to_rank --bind_core_list 0-9,12-21,24-33,36-45,48-57,60-69,72-81,84-93 ds_comm_bench.py
@@ -28,4 +40,3 @@ deepspeed --bind_cores_to_rank --bind_core_list 0-9,12-21,24-33,36-45,48-57,60-6
 #deepspeed --force_multi --hostfile=hostfile.txt --launcher impi --bind_cores_to_rank --bind_core_list 0-9,12-21,24-33,36-45,48-57,60-69,72-81,84-93 ds_comm_bench.py
 #deepspeed --force_multi --hostfile=hostfile.txt --launcher impi --bind_cores_to_rank ds_comm_bench.py
 
-#mpirun -ppn 8 ~/oneCCL/build/_install/examples/benchmark/benchmark -i 17920 -y 5120 -d bfloat16
