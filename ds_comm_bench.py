30599db42f9bbdf3a4a6ef246fdee973a2f7bce2
Ma, Guokai <guokai.ma@intel.com>
2023-09-06 02:08:57 +0000

    clean up code, control oneCCL allreduce with USE_ONECCL=1 os.environ

--- ds_comm_bench.py
+++ ds_comm_bench.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import deepspeed
 import deepspeed.comm as dist
@@ -8,11 +9,7 @@ deepspeed.init_distributed()
 def alloc_tensors(use_dtype):
     a = torch.ones(1024, 1024, dtype=torch.bfloat16)
     c = torch.ones(1024, 1024, dtype=torch.bfloat16)
-    #t = torch.ones(6144, 4, dtype=use_dtype) * (dist.get_rank()+1.0)
-    #t = torch.ones(14336, 4, dtype=use_dtype) * (dist.get_rank()+1.0)
-    #t = torch.ones(1024, 10, dtype=use_dtype) * (dist.get_rank()+1.0)
-    t = torch.ones(1024, 5, dtype=use_dtype) * (dist.get_rank()+1.0)
-    #t = torch.ones(1024, 512, dtype=use_dtype) * (dist.get_rank()+1.0)
+    t = torch.ones(1024, 10, dtype=use_dtype) * (dist.get_rank()+1.0)
     return a, c, t
 
 def test_allreduce(reuse_buffer, use_dtype, loop_count):
@@ -24,31 +21,24 @@ def test_allreduce(reuse_buffer, use_dtype, loop_count):
     for i in range(loop_count):
         if not reuse_buffer:
             a,c,t = alloc_tensors(use_dtype)
-        #torch.matmul(a, b, out=c)
-        #torch.matmul(a, b, out=c)
-        #v = 0.0000001
-        #for i in range(100000):
-        #    v = v + 0.01
+        torch.matmul(a, b, out=c)
         dist.barrier(t)
         t0 = time.time()
-        dist.inference_all_reduce(t, async_op=False)
-        #dist.all_reduce(t)
+        if os.environ.get('USE_ONECCL') == '1':
+            dist.all_reduce(t)
+        else:
+            dist.inference_all_reduce(t, async_op=False)
         t1 = time.time()
         if (i==0 and dist.get_rank()==0):
             print (t)
         t_total += t1-t0
     end = time.time()
-    #print (f'total elapsed {end-start}')
     return t_total
 
-loop_count = 579200
+loop_count = 10000
 t0 = time.time()
 t = test_allreduce(False, torch.bfloat16, loop_count)
 t1 = time.time()
-#t = test_allreduce(False, torch.float32, loop_count)
-#t = test_allreduce(False, torch.float16, loop_count)
 
 if dist.get_rank() == 0:
     print (f'avg duration = {t/(loop_count/1000.0)}ms, E2E={t1-t0}')
-
-#dist.log_summary(show_straggler=True)
